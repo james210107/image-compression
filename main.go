a723bcca89030a4eaf9c28a3a82a51d7b6134fed
Mask0305 <mask210103@tyr-tech.com>
2021-11-11 12:02:58 +0800

    feat(progressbar): Remove bar add time

--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 	"io/ioutil"
 	"os"
 	"sync"
+	"time"
 
 	vips "github.com/davidbyttow/govips/v2"
 	"github.com/pterm/pterm"
@@ -13,11 +14,19 @@ import (
 )
 
 func main() {
-	// Ω
-	pterm.DefaultBigText.WithLetters(
-		pterm.NewLettersFromStringWithStyle("P", pterm.NewStyle(pterm.FgCyan)),
-		pterm.NewLettersFromStringWithStyle("Term", pterm.NewStyle(pterm.FgLightMagenta))).
-		Render()
+	start := time.Now()
+	newHeader := pterm.HeaderPrinter{
+		TextStyle:       pterm.NewStyle(pterm.BgCyan),
+		BackgroundStyle: pterm.NewStyle(pterm.FgBlack),
+		Margin:          20,
+	}
+
+	// Print header.
+	newHeader.Println("開始執行")
+	//pterm.DefaultBigText.WithLetters(
+	//pterm.NewLettersFromStringWithStyle("Tyr", pterm.NewStyle(pterm.FgLightBlue)),
+	//pterm.NewLettersFromStringWithStyle("-ImageBroker", pterm.NewStyle(pterm.FgRed))).
+	//Render()
 	quality := flag.Int("q", 70, "Quality(int): default 70")
 	worker := flag.Int("w", 4, "Number of Goroutine run at the same time(int): default 4")
 	lossless := flag.Bool("l", false, "lossless")
@@ -28,7 +37,7 @@ func main() {
 	path, _ := os.Getwd()
 	files, _ := ioutil.ReadDir(path + "/in")
 	length := len(files)
-	MainBar := NewBar(int64(length))
+	//MainBar := NewBar(int64(length))
 
 	wgA := new(sync.WaitGroup)
 
@@ -65,36 +74,37 @@ func main() {
 
 			return nil, nil
 		}()
-		MainBar.Add(1)
+		//MainBar.Add(1)
 	}
 
 	wgA.Wait()
+
+	newHeader.Printfln(fmt.Sprintf("總花費秒數: %v秒", time.Now().Sub(start).Seconds()))
 }
 
 func NewBar(length int64) *progressbar.ProgressBar {
-	bar := progressbar.NewOptions64(length,
-		// 是否要顯示顏色
-		progressbar.OptionEnableColorCodes(true),
-		// 設置bar條長度
-		//progressbar.OptionSetWidth(50),
-		// 設置title
-		progressbar.OptionSetDescription("壓爆圖片中..."),
-		// 設置bar條樣式
-		progressbar.OptionSetTheme(progressbar.Theme{
-			// 進度條樣式
-			Saucer: "[yellow]-[reset]",
-			// 進度條的頭
-			SaucerHead: "[yellow]─=≡Σ((つ•̀ω•́)つ[reset]",
-			// 進度條還沒到的地方的樣式
-			SaucerPadding: " ",
-			// 進度條左邊框框
-			BarStart: "[",
-			// 進度條右邊框框
-			BarEnd: "]",
-		}),
-		//progressbar.OptionFullWidth(),
-		progressbar.OptionShowIts(),
-		progressbar.OptionSetItsString("張"),
-	)
+	//bar := progressbar.NewOptions64(length,
+	//// 是否要顯示顏色
+	//progressbar.OptionEnableColorCodes(true),
+	//// 設置title
+	//progressbar.OptionSetDescription("壓爆圖片中..."),
+	//// 設置bar條樣式
+	//progressbar.OptionSetTheme(progressbar.Theme{
+	//// 進度條樣式
+	//Saucer: "[yellow]-[reset]",
+	//// 進度條的頭
+	//SaucerHead: "[yellow]─=≡Σ((つ•̀ω•́)つ[reset]",
+	//// 進度條還沒到的地方的樣式
+	//SaucerPadding: " ",
+	//// 進度條左邊框框
+	//BarStart: "[",
+	//// 進度條右邊框框
+	//BarEnd: "]",
+	//}),
+	//progressbar.OptionShowIts(),
+	//progressbar.OptionSetItsString("張"),
+	//progressbar.OptionSetPredictTime(false),
+	//)
+	bar := progressbar.Default(length)
 	return bar
 }
